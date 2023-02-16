0779ac9b282897d5fc56d835c892e8363de4f05e
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-02-16 17:06:59 +0500

    Добавила задачу41

---
+++ zadacha41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задание	Пример
+// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.	0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+Console.WriteLine(String.Join(" ", GetFindPositive(baseArray)));
+
+int[] GetArrayFromString(string stringArray)
+{
+string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] res = new int[nums.Length];
+
+for (int i = 0; i < nums.Length; i++)
+{
+res[i] = int.Parse(nums[i]);
+}
+return res;
+}
+
+int GetFindPositive(int [] baseArray)
+{
+  int count = 0;
+  for (int i = 0; i < baseArray.Length; i++)
+  {
+    if ( baseArray[i] > 0) 
+    count++;
+  }
+  return count;
+}
